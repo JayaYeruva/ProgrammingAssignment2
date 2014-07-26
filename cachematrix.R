00ee95933a0fb42e913c878c5e512cb6f9ddf720
JayaYeruva <jayalakshmiy@gmail.com>
2014-07-26 18:39:46 +0530

    ad

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Input : matrix, that is invertible
+## Output : list of functions that opearte on the given matrix
 
-## Write a short comment describing this function
+## We need to collect the output of below function into an object.
+## That object is used to get Inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Input : list of functions returned by makeCacheMatrix()
+## Output : Inverse of the matrix, which was input to makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x$get()'
+	m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
